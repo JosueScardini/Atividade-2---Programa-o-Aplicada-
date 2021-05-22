75b9195414ae8e47e971e347de89e8c7c5333548
josuescardini <>
2021-05-22 02:24:45 +0000

    Pedindo informações para o usuário

--- main.cpp
+++ main.cpp
@@ -4,5 +4,17 @@
 using namespace std;
 
 int main() {
-  
+char L1, L2;
+string palavra, palavranova, P1, P2;
+int qtdtrocas;
+
+cout<< "Digite uma palavra "<<endl;
+cin>> palavra;  
+
+cout<< "A partir dessa palavra digite a letra a ser substituída "<<endl;
+cin>> L1;
+
+cout<< "Digite a letra que será colocada no lugar "<<endl;
+cin>>L2;
+
 }
